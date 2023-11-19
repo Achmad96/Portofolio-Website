c4f4c5febf5f78776dce65948cea598fc74d4f6a
Achmad96 <totsukamail3278@gmail.com>
2023-11-19 15:53:12 +0700

    modified files

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,7 +2,6 @@ import { allPosts } from "contentlayer/generated";
 import PostCard from "components/PostCard";
 export default function PostForm() {
     const posts = allPosts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-
     return (
         <div className="min-h-screen flex justify-center gap-5">
             {posts.map((post, idx) => {
--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -8,11 +8,11 @@ export default function PostCard(post: Post) {
     return (
         <div className="flex flex-col gap-5 w-80 items-center">
             <Link href={post.url}>
-                <Image src={thumbnail} alt="thumbnail" width={700} height={500} className="blur-lg hover:blur-0 transition-all duration-200" />
+                <Image src={thumbnail} alt="thumbnail" width={700} height={500} />
             </Link>
             <div className="flex flex-col">
                 <h2 className="mb-1 text-2xl">
-                    <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
+                    <Link href={post.url} className="font-bold text-blue-400 hover:text-blue-500 dark:text-blue-400">
                         {post.title}
                     </Link>
                 </h2>
