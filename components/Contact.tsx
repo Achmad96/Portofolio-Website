b26cce840f5c99d9688fe4091fe734d10ccf6d2d
Achmad96 <totsukamail3278@gmail.com>
2023-11-19 19:33:39 +0700

    modified Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,7 +9,7 @@ export default function Contact() {
         <div className="w-full h-screen flex justify-center items-center ">
             <form autoComplete="off" method="post" name="contactPerson" className="flex flex-col w-[50%] max-md:w-[80%] justify-start gap-5">
                 <h1 className="text-3xl">Please give me an advice &#129303;</h1>
-                <motion.label whileInView={{ x: 0, opacity: 1 }} initial={{ x: -100, opacity: 0 }}>
+                <motion.label initial={{ x: -100, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }}>
                     <input
                         placeholder="Enter your name..."
                         name="username"
@@ -17,26 +17,15 @@ export default function Contact() {
                         className="p-3 text-lg bg-transparent"
                         onKeyDown={e => {
                             if (e.key === "Enter") {
+                                setIndex(1);
                                 e.preventDefault();
                                 ref.current?.focus();
-                                setIndex(1);
                             }
                         }}
                     />
                 </motion.label>
-                <motion.label animate={index >= 1 ? { x: 0, opacity: 1 } : {}} initial={{ x: -100, opacity: 0 }}>
-                    <input
-                        ref={ref}
-                        placeholder="Enter your message..."
-                        name="messages"
-                        type="text"
-                        className="p-3 text-lg bg-transparent"
-                        onKeyDown={e => {
-                            if (e.key === "Enter") {
-                                submitRef.current?.click();
-                            }
-                        }}
-                    />
+                <motion.label initial={{ x: -100, opacity: 0 }} animate={index >= 1 ? { x: 0, opacity: 1 } : {}}>
+                    <input ref={ref} placeholder="Enter your message..." name="messages" type="text" className="p-3 text-lg bg-transparent" />
                 </motion.label>
                 <input ref={submitRef} type="submit" className="hidden" />
             </form>
