a3db93f0716cc72f26f771f8b4bbf129bae97c3a
Achmad96 <totsukamail3278@gmail.com>
2024-02-24 08:22:02 +0700

    modified works

--- components/ProjectsMetadata.ts
+++ components/ProjectsMetadata.ts
@@ -1,5 +1,8 @@
 import calculator from "../public/images/calculator.png";
 import todolist from "../public/images/todolist.png";
+import geotera from "../public/images/geotera.png";
+import spotimad from "../public/images/spotimad.png";
+
 export const Projects = [
   {
     title: "Calculator",
@@ -16,5 +19,21 @@ export const Projects = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     iconTypes: ["react", "css"],
     href: "https://todolist-app-three-jet.vercel.app/",
+  },
+  {
+    title: "Spotimad",
+    thumbnail: spotimad,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    iconTypes: ["nextjs", "tailwind"],
+    href: "https://spotimad.vercel.app/",
+  },
+  {
+    title: "Geotera",
+    thumbnail: geotera,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    iconTypes: ["nextjs", "tailwind"],
+    href: "https://geotera.vercel.app/",
   },
 ];
