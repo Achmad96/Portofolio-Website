5dab5a31e0afd0808c3f9f851d4da40ff2d31b77
Achmad96 <totsukamail3278@gmail.com>
2024-02-24 08:31:51 +0700

    modified about

--- components/About.tsx
+++ components/About.tsx
@@ -6,40 +6,40 @@ export default function About() {
     <div className="ml-60 flex flex-col gap-3 max-md:ml-20 max-sm:ml-5">
       <div className="flex flex-col gap-2">
         <motion.p
-          initial={{
-            opacity: 0,
-            y: 50,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
+          // initial={{
+          //   opacity: 0,
+          //   y: 50,
+          // }}
+          // animate={{
+          //   opacity: 1,
+          //   y: 0,
+          // }}
           className="text-2xl font-bold text-teal-200"
         >
           Hi, my name is
         </motion.p>
         <motion.h1
-          initial={{
-            opacity: 0,
-            y: 50,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
+          // initial={{
+          //   opacity: 0,
+          //   y: 50,
+          // }}
+          // animate={{
+          //   opacity: 1,
+          //   y: 0,
+          // }}
           className="text-6xl font-bold max-md:text-4xl"
         >
           Achmad Raihan
         </motion.h1>
         <motion.h2
-          initial={{
-            opacity: 0,
-            y: 50,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
+          // initial={{
+          //   opacity: 0,
+          //   y: 50,
+          // }}
+          // animate={{
+          //   opacity: 1,
+          //   y: 0,
+          // }}
           className="text-3xl font-bold max-md:text-2xl"
         >
           a tech enthusiast
