eb5bd93672cabb8c6ad337145f36424268903241
Achmad96 <totsukamail3278@gmail.com>
2024-02-24 10:43:17 +0700

    modified files

--- components/ProjectsMetadata.ts
+++ components/ProjectsMetadata.ts
@@ -5,22 +5,6 @@ import spotimad from "../public/images/spotimad.png";
 
 export const Projects = [
   {
-    title: "Calculator",
-    thumbnail: calculator,
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    iconTypes: ["react", "css"],
-    href: "https://calculator-web-umber.vercel.app/",
-  },
-  {
-    title: "Todolist",
-    thumbnail: todolist,
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    iconTypes: ["react", "css"],
-    href: "https://todolist-app-three-jet.vercel.app/",
-  },
-  {
     title: "Spotimad",
     thumbnail: spotimad,
     description:
@@ -35,5 +19,21 @@ export const Projects = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     iconTypes: ["nextjs", "tailwind"],
     href: "https://geotera.vercel.app/",
+  },
+  {
+    title: "Calculator",
+    thumbnail: calculator,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    iconTypes: ["react", "css"],
+    href: "https://calculator-web-umber.vercel.app/",
+  },
+  {
+    title: "Todolist",
+    thumbnail: todolist,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    iconTypes: ["react", "css"],
+    href: "https://todolist-app-three-jet.vercel.app/",
   },
 ];
