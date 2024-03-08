41af412cf5bcd70000bf4bfefe7da399a046ff0f
Achmad96 <totsukamail3278@gmail.com>
2024-03-08 20:37:59 +0700

    remove spotimad from projects list

--- constants/ProjectsMetadata.ts
+++ constants/ProjectsMetadata.ts
@@ -1,7 +1,6 @@
 import calculator from "../public/images/calculator.png";
 import todolist from "../public/images/todolist.png";
 import geotera from "../public/images/geotera.png";
-import spotimad from "../public/images/spotimad.png";
 
 export const Projects = [
   {
@@ -11,15 +10,6 @@ export const Projects = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     iconTypes: ["nextjs", "tailwind"],
     href: "https://geotera.vercel.app/",
-    type: "Website",
-  },
-  {
-    title: "Spotimad",
-    thumbnail: spotimad,
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    iconTypes: ["react", "tailwind"],
-    href: "https://spotimad.vercel.app/",
     type: "Website",
   },
   {
